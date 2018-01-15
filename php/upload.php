0ed37ce12ec743eccc9b27e91ae500d17db22ed7
ShinyHero <11gxf@tongji.edu.cn>
2018-01-15 14:46:25 +0800

    add my own php folder

---
+++ php/upload.php
@@ -0,0 +1,6 @@
+<?php
+header('Access-Control-Allow-Origin: *'); //设置http://www.baidu.com允许跨域访问
+// header("Content-Type: text/html; charset=utf-8");
+// header('Access-Control-Allow-Headers: X-Requested-With,X_Requested_With'); //设置允许的跨域header
+  return "Hello"
+?>
